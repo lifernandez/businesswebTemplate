1a2abb985f67c76f5df3c8519d8566be95d43d33
Linda Fernandez <fernandez.linda.cristal@gmail.com>
2025-08-31 11:37:16 -0700

    tweak on getInTouch js

--- getInTouch.js
+++ getInTouch.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   fetch("getInTouch.html")
     .then(response => response.text())
     .then(data => {
-      document.getElementById("contactForm").innerHTML = data;
+      document.getElementById("contact").innerHTML = data;
     })
     .catch(error => console.error("Error loading Contact Form:", error));
 });
